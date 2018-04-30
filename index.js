1552a722ba4d8143ce1c72839ecf427c859f1bd0
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-29 22:05:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const discord = require('discord.js')
 const client = new discord.Client();
 
 client.on('ready', () => console.log('CONNECTED TO ' + client.user.username))
-client.on('message', async msg => {
-	if(msg.author.id !== '284137818895417344') return;
-	console.log(`${msg.author.username} said ${msg.content} in ${msg.channel.name}.`)
-	if(msg.content === '.ping') {
-        msg.delete();
-        const msg = await message.channel.send('Loading...')
-        msg.edit('Ping: **' + (msg.createdAt - message.createdAt) + 'ms**');
-    }
+client.on('message', async message => {
+	if(message.author.id !== '284137818895417344') return;
+	console.log(`${message.author.username} said ${message.content} in ${message.channel.name}.`)
+	if(message.content === '.ping') {
+    message.delete();
+    const msg = await message.channel.send('Loading...')
+    msg.edit('Ping: **' + (msg.createdAt - message.createdAt) + 'ms**');
+  }
 })
  
 client.login(process.env.BOT_TOKEN);
