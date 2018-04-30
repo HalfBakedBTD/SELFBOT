d61702b87f74eb7947c687169f87c09c0d3698d5
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-30 17:50:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,34 +32,34 @@ client.on('guildDelete', guild => {
 client.on('message', async message => {
 	if(message.author.id !== '284137818895417344') return;
 	console.log(`${message.author.username} said ${message.content} in #${message.channel.name}.`)
-	if(message.content === '.ping') {
+	if(message.content === 'ping') {
     message.delete();
     const msg = await message.channel.send('Loading...')
     msg.edit('Ping: **' + (msg.createdAt - message.createdAt) + 'ms**');
   }
-	if(message.content === '.members') {
+	if(message.content === 'members') {
     message.delete();
     const msg = await message.channel.send('Loading...')
     msg.edit(`Server Members: ${message.guild.memberCount}`);
   }
-	if(message.content.startsWith('.d')) {
+	if(message.content.startsWith('d')) {
     message.delete();
   }
-	if(message.content === '.help') {
+	if(message.content === 'help') {
     message.delete();
 		client.channels.filter(c => c.id === '440364762505805845').forEach(channel => {
     	channel.send("**My Commands:**\n`.help` - gives help...\n`.ping` - pings me.\n`.d` - deletes the message.\n`.members` - gives membercount.");
 		});
   }
-	if(message.content.startsWith('.tu')) {
+	if(message.content.startsWith('tu')) {
     message.delete();
 		message.channel.send("👍")
   }
-	if(message.content.startsWith('.td')) {
+	if(message.content.startsWith('td')) {
     message.delete();
 		message.channel.send("👎")
   }
-	if(message.content.startsWith('.tow')) {
+	if(message.content.startsWith('tow')) {
     message.delete();
 		message.channel.send("😜")
   }
