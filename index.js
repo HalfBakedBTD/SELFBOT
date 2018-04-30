6e2ae3a6892062633dd73bdb547dcfcaea1a5b4a
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-30 00:29:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ const client = new discord.Client();
 let chat = require("./chats.json");
 
 client.on('ready', () => console.log('CONNECTED TO ' + client.user.username))
+
+cleint.on('guildCreate', guild => {
+  client.channels.filter(c => c.id === '440364762505805845').forEach(channel => {
+    let botAvatar = client.user.displayAvatarURL;
+    let eventEmbed = new Discord.RichEmbed()
+    .setColor('#27ae60')
+    .setThumbnail(botAvatar)
+    .setDescription(`**Event:** added to a guild.\n**User Guilds Size:** ${client.guilds.size}\n**Guild:** ${guild.name}\n**Owner**: ${guild.owner}\n**Owner ID:** ${guild.ownerID}\n**Members:** ${guild.memberCount}\n**Region:** ${guild.region}\n**Verification Level:** ${guild.verificationLevel}`);
+  
+   channel.send(eventEmbed);
+  });
+});
+
+client.on('guildDelete', guild => {
+  client.channels.filter(c => c.id === '440364762505805845').forEach(channel => {
+    let botAvatar = client.user.displayAvatarURL;
+    let eventEmbed = new Discord.RichEmbed()
+    .setColor('#e74c3c')
+    .setThumbnail(botAvatar)
+    .setDescription(`**Event:** removed from a guild.\n**User Guilds Size:** ${client.guilds.size}\n**Guild:** ${guild.name}\n**Owner**: ${guild.owner}\n**Owner ID:** ${guild.ownerID}\n**Members:** ${guild.memberCount}\n**Region:** ${guild.region}\n**Verification Level:** ${guild.verificationLevel}`);
+  
+   channel.send(eventEmbed);
+  });
+});
+
 client.on('message', async message => {
 	if(message.author.id !== '284137818895417344') return;
 	console.log(`${message.author.username} said ${message.content} in #${message.channel.name}.`)
